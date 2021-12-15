e5583c08dc998978d27f9f9776165955faa25dea
JoahDeVries <jcdevries@bsu.edu>
2021-12-14 20:06:47 -0500

    Security updates

--- db_config.js
+++ db_config.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "password",
+    user: "[INSERT USERNAME HERE]",
+    password: "[INSERT PASSWORD HERE]",
     database: "AISTestData"
 });
 
